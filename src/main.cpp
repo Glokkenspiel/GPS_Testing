1528e7a691428d7cb6b01da8264acdc1243e559c
Glokkenspiel <wilfriedelek@gmail.com>
2022-03-16 16:11:07 -0400

    MK1 of GPS code complete, I have yet to implement the Kalamin Filter (spelling is probably off)

--- src/main.cpp
+++ src/main.cpp
@@ -8,11 +8,144 @@
 /*----------------------------------------------------------------------------*/
 
 // ---- START VEXCODE CONFIGURED DEVICES ----
+// Robot Configuration:
+// [Name]               [Type]        [Port(s)]
+// GPS                  gps           20              
+// leftBase             motor_group   1, 2            
+// rightBase            motor_group   3, 4            
 // ---- END VEXCODE CONFIGURED DEVICES ----
 
 #include "vex.h"
+#include <cmath>
 
 using namespace vex;
+
+int tLeft = 0;
+int tRight = 1;
+
+
+
+float pheta1;
+float pheta2;
+float midA;
+
+int aQuadrant;
+int pQuadrant;
+
+bool lr;
+
+/*---Drive in a strait line---*/
+void baseS(directionType dir, float speed){
+  leftBase.spin(dir, speed, percent);
+  rightBase.spin(dir, speed, percent);
+}
+
+/*---Drive in a turn---*/
+void baseT(bool lORr, float speed){
+  if(lORr == 0){
+    leftBase.spin(fwd, speed, percent);
+    rightBase.spin(reverse, speed, percent);
+  }else{
+    leftBase.spin(reverse, speed, percent);
+    rightBase.spin(fwd, speed, percent);
+  }
+}
+
+/*---Drive in an Arc---*/
+void baseA(directionType dir, bool lORr, float speed, float offset){
+  if(lORr == 0){
+    leftBase.spin(dir, speed - offset, percent);
+    rightBase.spin(dir, speed, percent);
+  }else{
+    leftBase.spin(dir, speed, percent);
+    rightBase.spin(dir, speed - offset, percent);
+  }
+}
+
+void baseActT()
+
+/*---Stop the base---*/
+void baseST(){
+  leftBase.stop();
+  rightBase.stop();
+}
+
+///////////////////////////////////////////////////////////////////////////////////
+
+/*---Find another angle required for pheta---*/
+void findPheta2(){
+  if(GPS.heading() > 0 && GPS.heading() <= 90){
+    pheta2 = 90-GPS.heading();
+    aQuadrant = 1;
+  }else if(GPS.heading() > 90 && GPS.heading() <= 180){
+    pheta2 = GPS.heading()-90;
+    aQuadrant = 4;
+  }else if(GPS.heading() > 180 && GPS.heading() < 270){
+    pheta2 = 270-GPS.heading();
+    aQuadrant = 3;
+  }else{
+    pheta2 = GPS.heading()-270;
+    aQuadrant = 2;
+  }
+}
+
+/*---Find the amount needed to turn---*/
+void findPheta(float xTgt, float yTgt){
+  pheta1 = std::abs(atan((GPS.yPosition(mm)-yTgt)/(GPS.xPosition(mm)-xTgt))*180/M_PI);
+  findPheta2();
+}
+
+void findPQuadrant(float xTgt, float yTgt){
+  if(xTgt <= GPS.xPosition(mm) && yTgt <= GPS.yPosition(mm)){
+    pQuadrant = 3;
+  }else if(xTgt > GPS.xPosition(mm) && yTgt <= GPS.yPosition(mm)){
+    pQuadrant = 4;
+  }else if(xTgt <= GPS.xPosition(mm) && yTgt > GPS.yPosition(mm)){
+    pQuadrant = 2;
+  }else{
+    pQuadrant = 1;
+  }
+}
+
+/*---Do all the calculations for pheta---*/
+void usePheta(float xTgt, float yTgt){
+  findPQuadrant(xTgt, yTgt);
+  if(pQuadrant == 1){
+    midA = 90-pheta1;
+  }else if(pQuadrant == 2){
+    midA = 270+pheta1;
+  }else if(pQuadrant == 3){
+    midA = 270-pheta1;
+  }else{
+    midA = 90+pheta1;
+  }
+}
+
+/*---Find the angle needed to turn to---*/
+void findA(float xTgt, float yTgt){
+  findPheta(xTgt, yTgt);
+  usePheta(xTgt, yTgt);
+}
+
+void turnDirection(){
+  if(((aQuadrant+1 || aQuadrant-3) == pQuadrant) || (aQuadrant == pQuadrant && GPS.heading() >= midA) || ((aQuadrant+2 == pQuadrant || aQuadrant-2 == pQuadrant) && (GPS.heading()+180 <= midA || GPS.heading()-180 <= midA))){
+    lr = tLeft;
+  }else{
+    lr = tRight;
+  }
+}
+
+/*---Turn to point---*/
+void tTP(float xTgt, float yTgt){
+  findA(xTgt, yTgt);
+  turnDirection();
+  actTurn(midA, 100);
+}
+
+/*---Drive to a point---*/
+void dTP(float xTgt, float yTgt, float aTgt){
+  tTP(xTgt, yTgt);
+}
 
 int main() {
   // Initializing Robot Configuration. DO NOT REMOVE!
--- src/robot-config.cpp
+++ src/robot-config.cpp
@@ -1,15 +1,30 @@
 #include "vex.h"
 
 using namespace vex;
+using signature = vision::signature;
+using code = vision::code;
 
-// A global instance of brain used for printing to the V5 brain screen
-brain Brain;
+// A global instance of brain used for printing to the V5 Brain screen
+brain  Brain;
+
+// VEXcode device constructors
+gps GPS = gps(PORT20, 0.00, 0.00, mm, 180);
+motor leftBaseMotorA = motor(PORT1, ratio18_1, false);
+motor leftBaseMotorB = motor(PORT2, ratio18_1, false);
+motor_group leftBase = motor_group(leftBaseMotorA, leftBaseMotorB);
+motor rightBaseMotorA = motor(PORT3, ratio18_1, true);
+motor rightBaseMotorB = motor(PORT4, ratio18_1, true);
+motor_group rightBase = motor_group(rightBaseMotorA, rightBaseMotorB);
+
+// VEXcode generated functions
+
+
 
 /**
  * Used to initialize code/tasks/devices added using tools in VEXcode Pro.
- *
+ * 
  * This should be called at the start of your int main function.
  */
-void vexcodeInit(void) {
-  // Nothing to initialize
+void vexcodeInit( void ) {
+  // nothing to initialize
 }
